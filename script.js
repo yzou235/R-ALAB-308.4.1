47e8fecda0c7ded8ba990574ee8ee82d92b72df2
yzou235 <yumingzou235@gmail.com>
2023-11-29 18:05:35 -0500

    Solved part 3. Created objects for each row and stored these objects in an array.

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-// Part 1: Refactoring Old Code
+// Part 2: Expanding Functionality
 
 const csvString = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
 
 const rows = csvString.split("\n");
 console.log(rows);
 
-let newArray = [];
+let result1 = [];
 
 rows.forEach((row) => {
-    newArray.push(row.split(","))
+    result1.push(row.split(","))
 });
-console.log(newArray);
+console.log(result1);
+
+// Part 3: Transforming Data
+
+const keys = result1[0];
+
+let result2 = [];
+
+for(let i = 1; i < result1.length; i++) {
+    const rowObject = {};
+    for (let j = 0; j < keys.length; j++) {
+        rowObject[keys[j]] = result1[i][j];
+    }
+    result2.push(rowObject);
+}
+
+console.log(result2);
